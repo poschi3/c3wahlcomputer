f5ec8ad4bc6e42b562a96f17f496bbad38ba2b8f
Poschi <development@poschi.net>
2019-11-07 14:46:05 +0100

    Initial comit with static page

---
+++ site/script.js
@@ -0,0 +1,12 @@
+function feel(feeling) {
+    console.log(feeling);
+
+    // // HTTP-POST
+    // let request = new XMLHttpRequest();
+    // request.open('POST', '/api.php', true);
+    // request.setRequestHeader('Content-Type', 'application/json');
+    // let body = {
+    //     "feeling": feeling
+    // }
+    // request.send(JSON.stringify(body));
+}
