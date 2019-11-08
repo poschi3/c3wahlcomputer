4e48fe6dbf9ae2e3fbca677009ce8c807eaaccea
Poschi <development@poschi.net>
2019-11-08 19:39:14 +0100

    Redirect after submit

--- site/script.js
+++ site/script.js
@@ -3,6 +3,11 @@ function feel(feeling) {
 
     // HTTP-POST
     let request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', function (e) {
+        if (this.readyState === 4) {
+            window.location.href = "https://show.feel.poschi.net/";
+        }
+    });
     request.open('POST', '/api.php', true);
     request.setRequestHeader('Content-Type', 'application/json');
     let body = {
